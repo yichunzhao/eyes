d2b6a4863f73b672459e6dc056d2ae67a62912c3
yichunzhao <zhaoyichun@hotmail.com>
2021-05-03 04:35:18 +0200

    Initial commit

---
+++ eyes-config-server/src/main/java/com/ynz/demo/eyesconfigserver/EyesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.eyesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyesConfigServerApplication.class, args);
+	}
+
+}
